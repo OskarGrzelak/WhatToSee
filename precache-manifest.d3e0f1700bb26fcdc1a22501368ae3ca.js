95bf60b227bd042fe0ea69f92f74676d66317af6
OskarGrzelak <oskargrzelak@gmail.com>
2019-06-25 13:05:16 +0200

    Updates

---
+++ precache-manifest.d3e0f1700bb26fcdc1a22501368ae3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e0118278d3de3702ac4dce01b8ac30",
+    "url": "/WhatToSee/index.html"
+  },
+  {
+    "revision": "f6e92d4881167cc5a631",
+    "url": "/WhatToSee/static/css/main.a8edb945.chunk.css"
+  },
+  {
+    "revision": "c12f5bbf937910035c66",
+    "url": "/WhatToSee/static/js/2.fc156c72.chunk.js"
+  },
+  {
+    "revision": "f6e92d4881167cc5a631",
+    "url": "/WhatToSee/static/js/main.877d0283.chunk.js"
+  },
+  {
+    "revision": "c2684d5c745b65d2a8af",
+    "url": "/WhatToSee/static/js/runtime~main.78579115.js"
+  },
+  {
+    "revision": "1e663c1576698e2d9d19c496ece990ea",
+    "url": "/WhatToSee/static/media/no-avalible-image.1e663c15.png"
+  }
+]);
